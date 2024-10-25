db07077d4388857c3b676cad09c51da99e843af9
windyeasy <windyeasy13gb@163.com>
2024-10-25 18:59:37 +0800

    feat: 更新封装请求方法

--- src/services/request/index.js
+++ src/services/request/index.js
@@ -1,45 +1,171 @@
-
+export function objectToQueryString(obj) {
+  return Object.keys(obj)
+    .map((key) => {
+      return encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]);
+    })
+    .join("&");
+}
 
 class WdRequest {
-  /**
-    * 添加构造配置
-    * @constructor 
-    * @param {object} config       
-    * @param {string} config.baseUrl            - 请求的公共url地址
-    * @param {number} config.timeout            - 请求超时时间
-    * @param {object} config.interceptor          - 请求拦截器
-    * @param {?function} config.interceptor.requestSuccessFn  - 请求成功拦截器
-    * @param {?function} config.interceptor.responseSuccessFn - 响应成功拦截器
-    * @returns 
-    */
+  config;
+  url;
+
   constructor(config) {
-    this.config = config
+    this.config = config;
   }
+
+  _fetchUrl(url) {
+    if (url.includes("http")) {
+      this.url = url;
+    } else {
+      this.url = this.config.baseURL + url;
+    }
+    return this.url;
+  }
+
   request(config) {
-    return new Promise((reslove, reject) => {
-      config.url = this.config.baseUrl + config.url
-      // 实现请求拦截
+  
+    return new Promise((resolve, reject) => {
+      config.url = this._fetchUrl(config.url);
+
+      // 实现全局请求拦截
       if (this.config?.interceptor?.requestSuccessFn) {
-        config = this.config.interceptor.requestSuccessFn(config)
+        config = this.config.interceptor.requestSuccessFn(config);
       }
+      // 实现局部请求拦截
+      if (config.interceptor?.requestSuccessFn) {
+        config = config.interceptor.requestSuccessFn(config);
+      }
+
+      // 解析query方法
+      if (config.query) {
+        const queryStr = objectToQueryString(config.query);
+        if (config.url.includes("?")) {
+          config.url += `&${queryStr}`;
+        } else {
+          config.url += `?${queryStr}`;
+        }
+      }
+      
       uni.request({
         timeout: this.config.timeout, // 延迟时间
-        dataType: 'json',
-        responseType: 'json',
-        ...config, // 可以通过重写配置覆盖原有配置
+        dataType: "json",
+        url: config.url,
+        ...config,
         success: (res) => {
-          // 实现响应拦截
-          if (this.config?.interceptor?.responseSuccessFn) {
-            res = this.config.interceptor.responseSuccessFn(res)
+          // 有可能在执行的过程出现异常后抛出异常
+          try {
+            // 实现全局响应拦截
+            if (this.config?.interceptor?.responseSuccessFn) {
+              res = this.config.interceptor.responseSuccessFn(res);
+            }
+            // 实现局部响应拦截
+            if (config?.interceptor?.responseSuccessFn) {
+              res = config.interceptor.responseSuccessFn(res);
+            }
+            resolve(res);
+          } catch (error) {
+            reject(error);
           }
-          reslove(res)
         },
         fail: (error) => {
-          reject(error)
+          if (this.config?.interceptor?.responseErrorFn) {
+            error = this.config.interceptor.responseErrorFn(error);
+          }
+          if (config?.interceptor?.responseErrorFn) {
+            error = config?.interceptor?.responseErrorFn(error);
+          }
+          reject(error);
         },
-      })
-    })
+      });
+    });
+  }
+
+  get(url, data, config) {
+    return this.request({
+      url,
+      method: "GET",
+      data,
+      ...config,
+    });
+  }
+
+  post(url, data, config) {
+    return this.request({
+      url,
+      method: "POST",
+      data,
+      ...config,
+    });
+  }
+
+  put(url, data, config) {
+    return this.request({
+      url,
+      method: "POST",
+      data,
+      ...config,
+    });
+  }
+
+  delete(url, data, config) {
+    return this.request({
+      url,
+      method: "DELETE",
+      data,
+      ...config,
+    });
+  }
+
+  // 文件上传
+  uploadFile(config) {
+    return new Promise((resolve, reject) => {
+      config.url = this._fetchUrl(config.url);
+
+      // 实现全局请求拦截
+      if (this.config?.interceptor?.requestSuccessFn) {
+        config = this.config.interceptor.requestSuccessFn(config);
+      }
+      // 实现局部请求拦截
+      if (config.interceptor?.requestSuccessFn) {
+        config = config.interceptor.requestSuccessFn(config);
+      }
+
+      // 解析query方法
+      if (config.query) {
+        const queryStr = objectToQueryString(config.query);
+        if (config.url.includes("?")) {
+          config.url += `&${queryStr}`;
+        } else {
+          config.url += `?${queryStr}`;
+        }
+      }
+
+      uni.uploadFile({
+        ...config,
+        success: (res) => {
+          res.data = JSON.parse(res.data);
+          // 实现全局响应拦截
+          if (this.config?.interceptor?.responseSuccessFn) {
+            res = this.config.interceptor.responseSuccessFn(res);
+          }
+          // 实现局部响应拦截
+          if (config?.interceptor?.responseSuccessFn) {
+            res = config.interceptor.responseSuccessFn(res);
+          }
+          resolve(res);
+        },
+        fail: (error) => {
+          if (this.config?.interceptor?.responseErrorFn) {
+            error = this.config.interceptor.responseErrorFn(error);
+          }
+          if (config?.interceptor?.responseErrorFn) {
+            error = config?.interceptor?.responseErrorFn(error);
+          }
+          reject(error);
+        },
+      });
+    });
   }
 }
-export default WdRequest
-const request = new WdRequest({ baseUrl, timeout })
+export default WdRequest;
